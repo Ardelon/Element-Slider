95f6fc1aaffdac7c24deee37198166b8fc3a0687
ardelon <akeskinw@gmail.com>
2022-01-22 17:13:25 +0300

    -

--- slider.js
+++ slider.js
@@ -3,15 +3,25 @@
 const mainContainer = document.getElementById("main-container");
 const goLeftButton = document.getElementById("go-left-button");
 const goRightButton = document.getElementById("go-right-button");
+const goUpButton = document.getElementById("go-up-button");
+const goDownButton = document.getElementById("go-down-button");
 
-const arr = [];
+const elementArray = [];
+const beltArray = [];
+
 for (i=0; i<31; i++) {
-    arr.push(i);
+    elementArray.push(`Element ${i}`);
 }
+
+for (j = 0; j < 31; j++) {
+    beltArray.push(`Belt ${j}`);
+}
+
+
 
 goLeftButton.addEventListener('click', (e) => {
     e.preventDefault();
-    slideVisionMethod('left')
+    slideVisionMethod2('left')
     goLeftButton.removeEventListener('click', (e) => {
         e.preventDefault()
     });
@@ -19,13 +29,30 @@ goLeftButton.addEventListener('click', (e) => {
 
 goRightButton.addEventListener('click', (e) => {
     e.preventDefault();
-    slideVisionMethod('right')
+    slideVisionMethod2('right')
     goRightButton.removeEventListener('click', (e) => {
         e.preventDefault()
     });
 });
 
-const slideVisionMethod = (direction, amount = 250) => {
+
+goUpButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    slideVisionMethod2('up')
+    goUpButton.removeEventListener('click', (e) => {
+        e.preventDefault()
+    });
+});
+
+
+goDownButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    slideVisionMethod2('down')
+    goDownButton.removeEventListener('click', (e) => {
+        e.preventDefault()
+    });
+});
+const slideVisionMethod1 = (direction, amount = 250) => {
 
     let i = 1;
     let frame = 50
@@ -58,12 +85,86 @@ const slideVisionMethod = (direction, amount = 250) => {
     }
 };
 
+const slideVisionMethod2 = (direction, amount = 250) => {
+
+    let frame = 64; 
+    let frameRate = 6; // miliseconds
+    let animationDuration = frame * frameRate // miliseconds
+    let interval;
+    let target;
+    clearInterval(interval);
+    const driver = {
+        'left' : () => {
+      
+            console.log('left');
+            target = Math.max(mainContainer.scrollLeft - amount, 0);
+      
+            interval = setInterval(() => {
+                mainContainer.scrollLeft = Math.max(mainContainer.scrollLeft - amount/frame, target)
+                if (mainContainer.scrollLeft === target) {
+                    clearInterval(interval)
+                }
+            }, frameRate);
+        },
+        'right' : () => {
+
+            console.log('right');
+            target = Math.min(mainContainer.scrollLeft + amount, mainContainer.scrollWidth - mainContainer.clientWidth)
+
+            interval = setInterval(() => {
+                mainContainer.scrollLeft = Math.min(mainContainer.scrollLeft + amount/frame, target)
+                if (mainContainer.scrollLeft === target) {
+                    clearInterval(interval)
+                }
+            }, frameRate);
+        },
+        'up' : () => {
+
+            console.log('up');
+            target = Math.max(mainContainer.scrollTop - amount, 0);
+
+            interval = setInterval(() => {
+                mainContainer.scrollTop = Math.max(mainContainer.scrollTop - amount/frame, target)
+                if (mainContainer.scrollTop === target) {
+                    clearInterval(interval)
+                }
+            }, frameRate);
+        },
+        'down' : () => {
+
+            console.log('down');
+            target = Math.min(mainContainer.scrollTop + amount, mainContainer.scrollHeight - mainContainer.clientHeight)
+
+            interval = setInterval(() => {
+                mainContainer.scrollTop = Math.min(mainContainer.scrollTop + amount/frame, target)
+                if (mainContainer.scrollTop === target) {
+                    clearInterval(interval)
+                }
+            }, frameRate);
+        }
+    }
+
+    const driverKeys = Object.keys(driver);
+    if (driverKeys.includes(direction)) {
+        driver[direction]();
+    } else {
+        console.log('No or wrong direction');
+    }
+}
+
 const generateElementsForVisionMethod = () => {
-    arr.forEach(number => {
-        const element = document.createElement("div");
-        element.classList.add("element");
-        element.innerText = number;
-        mainContainer.appendChild(element);
+    beltArray.forEach(beltValue => {
+
+        const belt = document.createElement("div");
+        belt.classList.add("belt");
+
+        elementArray.forEach(number => {
+            const element = document.createElement("div");
+            element.classList.add("element");
+            element.innerHTML = `<p>${beltValue} ${number}</p>`;
+            belt.appendChild(element);
+        });
+        mainContainer.appendChild(belt);
     });
 }
 
